dccc502209467c93d548a46aad85ff521e438a59
Alex <alexeusgr@gmail.com>
2023-06-02 10:12:47 +0200

    build: add build and run tools

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    // include: ['tests/new*.test.ts', ],
+    include: ['tests/*.test.ts', 'tests/*.test.js'],
+  },
+});
