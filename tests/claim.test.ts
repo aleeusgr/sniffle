c86efdfd62b819296f18719a4d794f22efbdec29
Alex <alexeusgr@gmail.com>
2023-06-09 15:05:30 +0200

    refactor: rename transaction building module

--- tests/claim.test.ts
+++ tests/claim.test.ts
@@ -18,7 +18,7 @@ import {
 	Value
 } from "@hyperionbt/helios";
 
-import {lockAda} from './src/lock-loan.ts';
+import {lockAda} from './src/lock.ts';
 import {claimAda} from './src/claim.ts';
 
 describe("lock ADA to be exchanged for an nft", async () => {
