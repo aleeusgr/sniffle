6c3bb9064d5ce68005793b113da337b890a61796
Alex <alexeusgr@gmail.com>
2023-06-09 08:21:03 +0200

    docs: remove comments

--- src/loan.js
+++ src/loan.js
@@ -11,12 +11,9 @@ enum Redeemer {
 
 func main(datum: Datum, redeemer: Redeemer, context: ScriptContext) -> Bool {
 	tx: Tx = context.tx;
-	// https://www.hyperion-bt.org/helios-book/lang/builtins/value.html?highlight=value#contains_policy
-	
 
     redeemer.switch {
         Cancel => {
-            // Check that the owner signed the transaction
             tx.is_signed_by(datum.creator).trace("VS1: ")
         },
         Claim => {
